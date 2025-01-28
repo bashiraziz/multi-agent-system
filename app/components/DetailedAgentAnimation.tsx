3b26c5c78376c08e9cb613f3e4029693a1856bcb
Bashir Aziz <bashiraziz@gmail.com>
2025-01-28 04:07:27 -0500

    fixed error

--- app/components/DetailedAgentAnimation.tsx
+++ app/components/DetailedAgentAnimation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion} from "framer-motion"
 import { UserIcon, CaptainIcon, TechIcon, HealthIcon, DataIcon, APIIcon, FinanceIcon } from "./AgentIcons"
 
 export function DetailedAgentAnimation() {
